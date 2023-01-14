105e5266afc81881ef2fd19fe15d0640c4795662
LecturerMick <73646892+LecturerMick@users.noreply.github.com>
2023-01-14 18:34:41 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,24 @@
 OLED.init(128, 64)
-if (tinkercademy.ADKeyboard(ADKeys.A, AnalogPin.P0)) {
-    OLED.writeStringNewLine("A")
-}
+OLED.writeString("5 Button Test")
 basic.forever(function () {
-	
+    if (tinkercademy.ADKeyboard(ADKeys.A, AnalogPin.P0)) {
+        OLED.clear()
+        OLED.writeStringNewLine("A")
+    }
+    if (tinkercademy.ADKeyboard(ADKeys.B, AnalogPin.P0)) {
+        OLED.clear()
+        OLED.writeStringNewLine("B")
+    }
+    if (tinkercademy.ADKeyboard(ADKeys.C, AnalogPin.P0)) {
+        OLED.clear()
+        OLED.writeStringNewLine("C")
+    }
+    if (tinkercademy.ADKeyboard(ADKeys.D, AnalogPin.P0)) {
+        OLED.clear()
+        OLED.writeStringNewLine("D")
+    }
+    if (tinkercademy.ADKeyboard(ADKeys.E, AnalogPin.P0)) {
+        OLED.clear()
+        OLED.writeStringNewLine("E")
+    }
 })
